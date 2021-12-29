d5881ab4ad7e5d6d25a8a9b5ee599c6d39fb090c
jagadsukma <96783564+jagadsukma@users.noreply.github.com>
2021-12-29 17:53:35 +0700

    Update alive.py

--- SkyzuRobot/modules/alive.py
+++ SkyzuRobot/modules/alive.py
@@ -5,8 +5,8 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from SkyzuRobot.events import register
-from SkyzuRobot import telethn as tbot
+from Darknesssss_bot.events import register
+from Darknesssss_bot import telethn as tbot
 
 
 PHOTO = "https://telegra.ph/file/56811b69cbcece20bbebf.jpg"
@@ -14,17 +14,17 @@ PHOTO = "https://telegra.ph/file/56811b69cbcece20bbebf.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Skyzu Robot.** \n\n"
+    TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Darknesssss_bot.** \n\n"
     TEXT += "❂ **I'm Working Properly** \n\n"
-    TEXT += f"❂ **My Master : [Skyzu](https://t.me/skyzu)** \n\n"
+    TEXT += f"❂ **My Master : [Darkness](https://t.me/Darkness)** \n\n"
     TEXT += f"❂ **Library Version :** `{telever}` \n\n"
     TEXT += f"❂ **Telethon Version :** `{tlhver}` \n\n"
     TEXT += f"❂ **Pyrogram Version :** `{pyrover}` \n\n"
     TEXT += "**Thanks For Adding Me Here ❤️**"
     BUTTON = [
         [
-            Button.url("ʜᴇʟᴘ​", "https://t.me/SkyzuRobot?start=help"),
-            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/skyzusupport"),
+            Button.url("ʜᴇʟᴘ​", "https://t.me/Darknesssss_bot?start=help"),
+            Button.url("sᴜᴘᴘᴏʀᴛ​", "https://t.me/DraknessSuporrt"),
         ]
     ]
     await tbot.send_file(event.chat_id, PHOTO, caption=TEXT, buttons=BUTTON)
